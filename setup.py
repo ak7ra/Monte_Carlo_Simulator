45c3b1fb1ec421380a2447f08f35f165bdd969a9
Ami Kano <amikano@Amis-MacBook-Air.local>
2022-07-13 14:21:36 -0400

    submission

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'Monte Carlo Simulator',
+      version = '0.1',
+      description = 'A package containing the Die class, the Game class and the Analyzer class.',
+      url = 'https://github.com/ak7ra/Monte_Carlo_Simulator',
+      author = 'Ami Kano',
+      author_email = 'ak7ra@virginia.edu',
+      license = 'MIT',
+      packages = ['montecarlo'])
